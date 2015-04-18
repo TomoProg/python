5dab635842e429334241a8f5280a5ce3b519e3a8
TomoProg <helloworld0306.xxx@gmail.com>
2015-04-18 10:22:42 +0900

    new file commit

---
+++ util/my_urllib.py
@@ -0,0 +1,40 @@
+#-*- encode:utf-8 -*-
+
+import urllib.request
+
+def GetHTMLString(url):
+	""" 指定したURLのHTML文書を取得する """
+	try:
+		url_data = urllib.request.urlopen(url)
+		str_html = url_data.read()
+		chara_code = JudgeCharaCode(str_html)
+		if chara_code is not None:
+			str_html = str_html.decode(chara_code)
+		else:
+			str_html = None
+	except:
+		str_html = None
+
+	return str_html
+
+def JudgeCharaCode(str_data):
+	""" HTMLの文字コードを判定する """
+	chara_code_list = ["utf-8", "cp932", "euc-jp", "iso2022_jp", "shift-jis"]
+	flag = False
+	for chara_code in chara_code_list:
+		try:
+			str_data.decode(chara_code)
+			flag = True
+			break
+		except:
+			pass
+
+	if flag:
+		return chara_code
+	else:
+		return None
+		
+		
+if __name__ == "__main__":
+	html = GetHTMLString('http://www.yahoo.co.jp/')
+	print(html)
---
+++ util/timestamp.py
@@ -0,0 +1,26 @@
+#-*- encode:utf-8 -*-
+
+import datetime
+
+def GetNowYMDHMSStamp():
+	""" YYYYMMDDHHMMSS """
+	dt = datetime.datetime.now()
+	timestamp = dt.strftime('%Y%m%d%H%M%S')
+	return timestamp
+
+def GetNowYMDStamp():
+	""" YYYYMMDD """
+	dt = datetime.datetime.now()
+	timestamp = dt.strftime('%Y%m%d')
+	return timestamp
+
+def GetNowHMDStamp():
+	""" HHMMSS """
+	dt = datetime.datetime.now()
+	timestamp = dt.strftime('%H%M%S')
+	return timestamp
+
+if __name__ == "__main__":
+	print(GetNowYMDHMSStamp())
+	print(GetNowYMDStamp())
+	print(GetNowHMDStamp())
