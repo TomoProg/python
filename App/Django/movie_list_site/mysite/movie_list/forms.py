139d06e861b10d02f582bf2c79b5bfbd6207e0d1
TomoProg <helloworld0306.xxx@gmail.com>
2016-05-28 19:51:12 +0900

    Django remove

--- App/Django/movie_list_site/mysite/movie_list/forms.py
+++
@@ -1,9 +0,0 @@
-#-*- coding:utf-8 -*-
-from django.forms import ModelForm
-from movie_list.models import Movie
-
-class MovieForm(ModelForm):
-	''' 映画フォーム '''
-	class Meta:
-		model = Movie
-		fields = ('title', 'genre', 'num_stars', 'note', )
--- App/Django/test/mybook/cms/forms.py
+++
@@ -1,9 +0,0 @@
-#-*- coding:utf-8 -*-
-from django.forms import ModelForm
-from cms.models import Book
-
-class BookForm(ModelForm):
-	''' 書籍フォーム '''
-	class Meta:
-		model = Book
-		fields = ('name', 'publisher', 'page', )
